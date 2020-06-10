31e392bf5213190cb42a68558bff9cacb1f01151
rizwan230698 <rizwananwar230698@gmail.com>
2020-06-10 18:59:36 +0530

    added custom useForm hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export const useForm = (callback, initialState = {}) => {
+  const [state, setstate] = useState(initialState);
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setstate({ ...state, [name]: value });
+  };
+
+  const onSubmit = (...args) => {
+    callback(...args);
+  };
+
+  const resetForm = () => {
+    setstate({});
+  };
+
+  return [state, onChange, onSubmit, resetForm];
+};
